5d2dfe35e440f02c46cc2b3062faa265aef82d55
Михаил <sazanof.ru@yandex.ru>
2015-01-06 20:27:25 +0300

    Update modxplaning.class.php

--- assets/modules/modxplaning/classes/modxplaning.class.php
+++ assets/modules/modxplaning/classes/modxplaning.class.php
@@ -18,12 +18,12 @@ class MODxPlaning {
     public function getDefaultCalendar()
     {
         global $modx;
-        return $modx->db->getRow($modx->db->select("id,created,title,description,doc_id,def",$this->tbl_calendars,'WHERE def=1'));
+        return $modx->db->getRow($modx->db->select("id,created,title,description,doc_id,def",$this->tbl_calendars,"def=1"));
     }
     public function getCalendar($id)
     {
         global $modx;
-        return $modx->db->getRow($modx->db->select("id,created,title,description,doc_id,def",$this->tbl_calendars.'id='.$id));
+        return $modx->db->getRow($modx->db->select("id,created,title,description,doc_id,def",$this->tbl_calendars,"id=".$id));
     }
     public function getCalendars ()
     {
