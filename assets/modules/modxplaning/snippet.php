9171a05b11fb91133c12a405aa69c7d97db89379
Михаил <sazanof.ru@yandex.ru>
2016-09-08 22:00:58 +0300

    change js reg from header to footer

--- assets/modules/modxplaning/snippet.php
+++ assets/modules/modxplaning/snippet.php
@@ -1,101 +1,124 @@
 <?php
-/**
- * snippet MODxPlaning.
- * Author: sazanof
- * Date: 27.10.2014
- * Email: sazanof@gmail.com
- *
- * PARAMS
- * &cal_id - The calendar ID from DBASE
- */
-if(!defined('MODX_BASE_PATH')){die('What are you doing? Get out of here!');}
-include_once MODX_BASE_PATH.'assets/modules/modxplaning/inc/config.inc.php';
-include_once $mp_location.'classes/modxplaning.class.php';
-$cal = new MODxPlaning;
-$cal->tbl_calendars = $tbl_calendars;
-$cal->tbl_categories = $tbl_categories;
+/*
+Easy Newsletter 0.3
+Copyright by: Flux - www.simpleshop.dk
+update - SAZANOF - www.sazanof.ru
+---------------------------------------------------------------------*/
+function msqlResultRow ($sql,$field)
+{
+	global $modx;
+	$res = $modx->db->getRow($modx->db->query($sql));
+	return $res[$field];
+}
+$path = MODX_BASE_PATH.'/assets/modules/easynewsletter/';
+$manager_language = $modx->config['manager_language'];
+if ($modx->getLoginUserID()) {
+    $sql = "SELECT setting_name, setting_value FROM " . $modx->getFullTableName('user_settings') . " WHERE setting_name='manager_language' AND user=" . $modx->getLoginUserID();
+    $rs = $modx->db->query($sql);
+    if ($modx->db->getRecordCount($rs) > 0) {
+        $row = $modx->db->getRow($rs);
+        $manager_language = $row['setting_value'];
+    }
+}
+if (file_exists($path.'languages/' . $manager_language . '.php')) {
+    include($path.'languages/' . $manager_language . '.php');
+} else {
+    include($path.'languages/' . 'lang/english.php');
+}
+error_reporting(E_ALL ^ E_NOTICE);
 
-$defCal = $cal->getDefaultCalendar();
-$out = '';
-$jQ = isset($jQ) ? $jQ : 'n'; // default NO jQuery
-$calID = isset($calID) ? (int)$calID : $defCal['id']; // if empty echo def cal ID
-$isClick = isset($isClick) ? (int)$isClick : 0; // use if jquery ui dialog included
-$dayClick = isset($dayClick) ? (int)$dayClick : 0; // use if jquery ui dialog included
-$calHeight = isset($calHeight) ? (int)$calHeight : "'auto'"; // calendar Height
-$container = isset($container) ? $container : "#calendar"; // <div id="calendar">
-$result = isset($result) ? $result : "#result"; // <div id="result">
-$lang = isset($lang) ? $lang : 'ru';
-$successMsg = isset($successMsg) ? $modx->getChunk($successMsg) : '';
-if ($jQ=='y'){$modx->regClientStartupScript($mp_url.'js/jquery-1.11.1.min.js');}
-elseif ($jQ=='n'){$out.='';}
-else{die();}
-$js.='<script src="'.$mp_url.'fullcalendar/lib/moment.min.js"></script>
-<script src="'.$mp_url.'fullcalendar/fullcalendar.js"></script>
-<script src="'.$mp_url.'fullcalendar/lang-all.js"></script>';
-$js .= "<script>
-	$(document).ready(function() {
-	    var result = $('".$result."');
-		$('".$container."').fullCalendar({
-		firstDay: 1,
-		axisFormat:'HH:mm',
-		eventLimit: true,
-		height: ".$calHeight.",
-		header: {
-        left: 'prev,next today',
-				center: 'title',
-				right: 'month,agendaWeek,agendaDay'
-			},
-			lang: '".$lang."',";
-            if ($isClick==1)
-            {
-                $js.="
-                    eventClick: function(calEvent, jsEvent, view){
-                        $('#event_modal').dialog({
-                            modal:true,
-                            title:calEvent.title,
-                              open: function( event, ui ) {
-                                $('#event_modal').html('<div class=event_in_modal><span><b>Время проведения</b>' + moment(calEvent.start).format('DD.MM.YYYY HH:mm') + ' - '+ moment(calEvent.end).format('DD.MM.YYYY HH:mm') + '</span></div>');
-                              }
-                        });
-                    },
-                ";
-            }
-        if ($dayClick==1)
-            {
-                $js.="
-                    dayClick: function(date, allDay, jsEvent, view,start) {
-                        var newDate = moment(date).format('DD.MM.YYYY 08:mm');
-                        $('#s').val(newDate);
-                        $('#po').val(newDate);
-                        $('#dialog-form').dialog({title:'Заявка на фотосъемку',width:500,modal:true});
-                    },
-                ";
-            }
-			$js.="
-			/* источник записей */
-            eventSources: [{
-                url: '/assets/modules/modxplaning/inc/ajax.inc.php',
-                type: 'POST',
-                data: {
-                    cal_id:".$calID.",
-                    op: 'source'
-                },
-                success: function() {
-                    result.html('".$successMsg."');
-                },
+$action = $_POST['option'];
+$mode = isset($mode) ? $mode : 'subscribe';
+$cat_id = isset($cat_id) ? $cat_id : 0;
 
-                error: function() {
-                    result.html('Пусто!');
-                }
-            }]
-		});
-	});
-</script>";
-$modx->regClientCSS('<link href="'.$mp_url.'fullcalendar/fullcalendar.css" rel="stylesheet" />');
-$modx->regClientStartupScript($js);
-$container = str_replace ("#",'',$container);
-$result = str_replace ("#",'',$result);
-$out.='<div id="event_modal"></div>
-<div id="'.$result.'"></div>
-<div id="'.$container.'"></div>';
-echo $out;
+if ($mode == 'subscribe')
+{
+	$subscribe_button = $lang['subscribe'];
+}
+elseif ($mode=='unsubscribe')
+{
+	$subscribe_button = $lang['unsubscribe'];
+}
+$tpl = isset($tpl) ? $modx->getChunk($tpl) : '
+<div class="infos_subscr">
+	[+infos+]
+</div>
+<div id="subscr">
+<form id="subscrForm" onsubmit="return validate_form(this);" method="post">
+	<input type="hidden" value="" name="hid">
+	<input type="text" name="firstname"  value="" placeholder="[+lang_firstname+]" >
+  	<input type="text" name="lastname"  value="" placeholder="[+lang_lastname+]"  >
+	<input type="text" name="email"  value="" placeholder="[+lang_email+]">
+	<input type="hidden" name="op" value="set">
+	[+options_choice_suscribe+]
+	[+options_choice_unsuscribe+]
+<button type="submit" value="GO!" id="change" name="subscr_sbmt" class="button full-width">[+subscribe_button+]</button>
+</form>
+</div>';
+
+$infos = array();
+switch($mode) {
+case "subscribe":
+if ($_POST['hid']=='' and trim($_POST['email'])!=='')
+  {
+		$sql = "SELECT * FROM `easynewsletter_subscribers` WHERE email = '".trim($_POST['email'])."'";
+		$result = $modx->db->query($sql);
+		$num = $modx->db->getRecordCount($result);	
+		//если нет такого фдреса почты в БД, то записываем
+		if ($num < 1) 
+ 		 {
+				$sql = "INSERT INTO `easynewsletter_subscribers` VALUES ('', '".$modx->db->escape($_POST['firstname'])."', '".$modx->db->escape($_POST['lastname'])."', '".$modx->db->escape($_POST['email'])."', '', '', '', now(),'".$cat_id."') ";
+				if (trim($_POST['firstname'])!=='')
+				{				
+					if ($modx->db->query($sql))
+					{
+						$infos[] = $lang['subscribesuccess'];
+					}
+				}
+				else
+				{
+					$infos[] = $lang['infos_firstname'];
+				}
+		 } 
+		else 
+		{
+				$infos[] = $lang['alreadysubscribed'];
+		}
+  }
+	break;
+case "unsubscribe":
+	if ($_POST['email'])
+	{
+		$sql = "SELECT * FROM `easynewsletter_subscribers` WHERE email = '".$_POST['email']."'";
+		$result = $modx->db->query($sql);
+		$num = $modx->db->getRecordCount($result);	
+		if ($num < 1) {
+			$infos[] = $lang_notsubscribed;
+		} else {
+			$email = $_POST['email'];
+			$sql = "DELETE FROM easynewsletter_subscribers WHERE email = '".$email."'";
+			$result = $modx->db->query($sql);
+			$infos[] = $lang['unsubscribesuccess'];
+		}
+	}
+	break;  	
+}
+// если массив с сообщениями не пустой, то выводим его
+if (!empty($infos))
+{
+	$mes_wr = '<ul class="subscr_ul_mess">[+messages_wrapper+]</ul>';
+	foreach ($infos as $mes)
+	{
+		$mes = '<li>'.$mes.'</li>';
+	}
+	$out .= str_replace('[+messages_wrapper+]',$mes,$mes_wr);
+}
+$modx->setPlaceholder('mode',$mode);
+$modx->setPlaceholder('subscribe_button',$subscribe_button);
+$modx->setPlaceholder('lang_firstname',$lang['firstname']);
+$modx->setPlaceholder('lang_lastname',$lang['lastname']);
+$modx->setPlaceholder('lang_email',$lang['email']);
+$modx->setPlaceholder('infos',$mes_wr);
+$out .= $tpl;
+return $out;
+?>
